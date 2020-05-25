b744bc2a24639f9a72f0d100329d8fcce9096007
Sam Collette <samcollette@Sams-MacBook-Pro.local>
2020-05-25 16:09:14 -0700

    added bare bones data and functionality

---
+++ README.md
@@ -0,0 +1,18 @@
+# Creature Creator 5000
+
+## A basic drawing prompt generator.
+
+The goal of this project is to generate a prompt for creature/ character creation. Because, sometimes, you just need a bit of inspiration. 
+
+The output of the generator will provide one item out of each of the following groups:
+* Animals
+* Plant Life
+* Element Variables
+* Rocks and Minerals
+* Environments
+* Weapons
+* Equipment
+* Tools
+* Personality Traits
+
+Once you are given these variables, it is up to you what you create with them. 
---
+++ main.js
@@ -0,0 +1,357 @@
+const animals = [
+    "Mollusk",
+    "Crab / Lobster",
+    "Squid",
+    "Mudskipper",
+    "Fish-Deep Sea",
+    "Jellyfish", 
+    "Octopus",
+    "Fish-Fresh Water",
+    "Whale / Dolphin",
+    "Shell",
+    "Eel", 
+    "Leech",
+    "Coral / Anemone",
+    "Shark", 
+    "Stingray",
+
+    "Crocodile",
+    "Gila",
+    "Frog",
+    "Newt",
+    "Lizard",
+    "Snake",
+    "Turtle",
+
+    "Worm",
+    "Ant",
+    "Mosquito",
+    "Moth",
+    "Butterfly",
+    "Fly",
+    "Dragonfly",
+    "Lotus",
+    "Mantis",
+    "Bee, Wasp",
+    "Caterpillar",
+    "Beetle / Scarab",
+    "Flea, Mite",
+    "Spider",
+
+    "Wild Fowl",
+    "Farm Fowl",
+    "Seabird",
+    "City Bird",
+    "Tropical Bird",
+    "Bird of Prey",
+
+    "Sheep",
+    "Cow",
+    "Mouse / Rat",
+    "Rabbit",
+    "Pig, Boar",
+    "Deer / Pronghorn",
+    "Ram",
+    "Bull",
+    "Buck",
+    "Elephant",
+    "Giraffe",
+    "Bat",
+    "Bear",
+    "Lupine - Wild Dog",
+    "Horse",
+    "Zebra",
+    "Feline - Wild Cat",
+    "Primate",
+    "Wolf",
+    "Fox",
+    "Hedgehog",
+    "Opossum",
+    "Raccoon"
+]
+
+const plants = [
+    "Seaweed",
+    "Ferns",
+    "Desert Cacti",
+    "Jade",
+    "Domestic Flowers",
+    "Vines",
+    "Poppies",
+    "Grass / Dandelions",
+    "Bamboo",
+    "Wild Flowers",
+    "Carnivorous Plants",
+    "Asparagas",
+    "Pinecones",
+    "Berries",
+    "Ginger",
+    "Tree Fruit (Apple, Orange)",
+    "Beans",
+    "Gourds",
+    "Broccoli, Cauliflower",
+    "Artichokes",
+    "Corn",
+    "Grains / Wheat",
+    "Pineapples",
+    "Moss",
+    "Ooze / Jelly",
+    "Lichen",
+    "Mushroom",
+    "Willow Tree",
+    "Birch Tree",
+    "Maple Tree",
+    "Oak Tree",
+    "A Banyan",
+    "Pine Tree",
+    "Pine Tree"
+]
+
+const elements = [
+    "Fire",
+    "Electric Bolts",
+    "Embers",
+    "Molten Lava",
+    "Icicles",
+    "Fog", 
+    "Vapor",
+    "Waves",
+    "Dew Drops",
+    "Ripples",
+    "Frost / Snow",
+    "Suds / Bubbles",
+    "Tar / Gum",
+    "Wind - A Tornado"
+]
+
+const rocks = [
+    "Malachite",
+    "Mountain / Cliff Face",
+    "Brick",
+    "Cobblestone",
+    "Rust / Oxide",
+    "Cracked Clay",
+    "Stalactite / Stalagmite",
+    "Glass / Crystals",
+    "Powder / Sand",
+    "Slate / Shale",
+    "Cement / Sediment",
+    "Mercury / Chrome"
+]
+
+const environments = [
+    "Moon Cycles",
+    "Starfield",
+    "Crater / Asteroid",
+    "Solar Flare",
+    "Galaxy Form",
+    "Volcano",
+    "Planets",
+    "Cloud",
+    "Cyclone"
+]
+
+const weapons = [
+    "Club",
+    "Dagger",
+    "Greatclub",
+    "Handaxe",
+    "Javelin",
+    "Light Hammer",
+    "Mace",
+    "Quarterstaff",
+    "Sickle",
+    "Spear",
+    "Crossbow",
+    "Dart",
+    "Shortbow",
+    "Sling",
+    "Battleaxe",
+    "Flail",
+    "Glaive",
+    "Greataxe",
+    "Greatsword",
+    "Halberd",
+    "Lance",
+    "Longsword",
+    "Maul",
+    "Morningstar",
+    "Pike",
+    "Rapier",
+    "Scimitar",
+    "Shortsword",
+    "Trident",
+    "War Pick",
+    "Warhammer",
+    "Whip",
+    "Blowgun",
+    "Longbow",
+    "Net"
+]
+
+const equipmentItems = [
+    "Abacus",
+    "Vial of Acid",
+    "Flask of Alchemist's Fire",
+    "Ammunition (Arrows, Blowgun needles, Crossbow Bolts, Sling Bullets...etc)",
+    "Vial of Antitoxin/ Antidote",
+    "Arcane Focus",
+    "Backpack",
+    "Bag of Ball Bearings",
+    "Barrel",
+    "Basket",
+    "Bedroll",
+    "Bell",
+    "Blanket",
+    "Block and Tackle",
+    "Book",
+    "Glass Bottle",
+    "Bucket",
+    "Caltrops",
+    "Candle",
+    "Map/Scroll Case",
+    "Chain",
+    "Chalk",
+    "Chest",
+    "Climber's Kit",
+    "Costume",
+    "Fine Clothes",
+    "Component Pouch",
+    "Crowbar",
+    "Druidic Focus",
+    "Fishing Tackle",
+    "Flask or Tankard",
+    "Grappling Hook",
+    "Hammer",
+    "Sledge Hammer",
+    "Healer's Kit",
+    "Holy Symbol",
+    "Holy Water",
+    "Hourglass",
+    "Hunting Trap",
+    "Bottle of Ink",
+    "Ink Pen",
+    "Jug or Pitcher",
+    "Ladder",
+    "Lantern (Hooded, Paper)",
+    "Lamp",
+    "Lock",
+    "Magnifying Glass",
+    "Manacles",
+    "Mess Kit",
+    "Mirror",
+    "Flask of Oil",
+    "Parchment",
+    "Perfume Bottle",
+    "Mining Pick",
+    "Piton",
+    "Vial of Poison",
+    "Healing Potion",
+    "Pouch",
+    "Iron Pot",
+    "Robes",
+    "Rope (Hempen, Silk)",
+    "Merchant's Scale",
+    "Sealing Wax",
+    "Shovel",
+    "Signet Ring",
+    "Whistle",
+    "Shovel",
+    "Spyglass",
+    "Soap",
+    "Spellbook",
+    "Tent",
+    "Tinderbox",
+    "Torch",
+    "Vials",
+    "Waterskin",
+    "Whetstone",
+    "Tea Kettle",
+    "Umbrella",
+    "Key",
+    "Needle and Thread",
+    "Clock, Gears",
+    "Ractor Core",
+    "Bandages"
+]
+
+const tools = [
+    "Alchemist's Supplies",
+    "Brewer's Supplies",
+    "Calligrapher's Tools",
+    "Cartographer's Tools",
+    "Clockmaker's Kit",
+    "Cobbler's Tools",
+    "Cook's Utensils",
+    "Glassblower's Tools",
+    "Jewler's Tools",
+    "Leatherworker's Tools",
+    "Mason's Tools",
+    "Painter's Tools",
+    "Smith's Tools",
+    "Tinker's Tools",
+    "Weaver's Tools",
+    "Woodcarver's Tools",
+    "Disguise Kit",
+    "Forgery Kit",
+    "Gaming Set",
+    "Herbalism Kit",
+    "Musical Instrument",
+    "Navigator's Tools",
+    "Poisoner's Kit",
+    "Thieves' Tools",
+    "Vehicle/Mount"
+]
+
+const personalities = [
+    "Shy",
+    "Bold",
+    "Mischievous",
+    "Angry",
+    "Timid",
+    "Aloof",
+    "Fearful",
+    "Paranoid",
+    "Deadpan",
+    "Insane - Berserk",
+    "Insane - Happy",
+    "Jovial",
+    "Hysterical",
+    "Shifty",
+    "Confused"
+]
+
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+const addon = document.getElementById("addon");
+
+const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+const randomPlant = plants[Math.floor(Math.random() * plants.length)];
+const randomElement = elements[Math.floor(Math.random() * elements.length)];
+const randomRock = rocks[Math.floor(Math.random() * rocks.length)];
+const randomEnvironment = environments[Math.floor(Math.random() * environments.length)];
+const randomWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+const randomEquipment = equipmentItems[Math.floor(Math.random() * equipmentItems.length)];
+const randomTool = tools[Math.floor(Math.random() * tools.length)];
+const randomPersonality = personalities[Math.floor(Math.random() * personalities.length)];
+
+const defaultResults = ['<b>' + randomAnimal + '</b>' + " mixed with " + '<b>' + randomPlant + '</b>' + " made out of " + '<b>' + randomElement + '</b>' + " and " + '<b>' + randomRock + '</b>'];
+const addOns = [
+    '<ul class="list-group">' +
+    '<li class="list-group-item">' + '<b>Environment: </b>' + randomEnvironment + '</li>' +
+    '<li class="list-group-item">' + '<b>Weapon: </b>' + randomWeapon + '</li>' + 
+    '<li class="list-group-item">' + '<b>Equipment: </b>' + randomEquipment + '</li>'+ 
+    '<li class="list-group-item">' + '<b>Tool: </b>' + randomTool + '</li>' + 
+    '<li class="list-group-item">' + '<b>Personality Type: </b>' + randomPersonality + '</li>' +
+    '</ul>'
+]
+btn.addEventListener('click', getResult);
+
+function getResult() {
+    if (defaultResults){
+        result.innerHTML = '<span>A(n) </span>' + defaultResults;
+        addon.innerHTML = '<h3>Optional Things to Consider Adding: </h3>' + addOns;
+    } else {
+        alert("Oops");
+    }
+}
